aa2384d83aab7c1fcc8d3ed2679bda39868a7d3d
RookMG <tngks1995@naver.com>
2022-12-26 22:24:14 +0900

    [Silver IV] Title: 덱, Time: 248 ms, Memory: 20188 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        LinkedList<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch(command){
+                case "push_front":
+                    q.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "push_back":
+                    q.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop_front":
+                    sb.append(q.isEmpty()?-1+"\n":q.pop()+"\n");
+                    break;
+                case "pop_back":
+                    sb.append(q.isEmpty()?-1+"\n":q.pollLast()+"\n");
+                    break;
+                case "size":
+                    sb.append(q.size()+"\n");
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty()?1+"\n":0+"\n");
+                    break;
+                case "front":
+                    sb.append(q.isEmpty()?-1+"\n":q.peek()+"\n");
+                    break;
+                case "back":
+                    sb.append(q.isEmpty()?-1+"\n":q.peekLast()+"\n");
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
+}
