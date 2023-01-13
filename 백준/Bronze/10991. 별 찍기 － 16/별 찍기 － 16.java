6943279f4dd50e73430c98d450329b64bbaff868
RookMG <tngks1995@naver.com>
2023-01-13 14:58:43 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 136 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<n;i++)
+        	sb.append(" ");
+		sb.append("*").append("\n");
+        for(int i=1;i<n;i++) {
+        	for(int j=1;j<n-i;j++)
+        		sb.append(" ");
+    		sb.append("*");
+        	for(int j=0;j<2*i-1;j++)
+        		sb.append(j%2==0?" ":"*");
+    		sb.append("*");
+    		sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
