7a872a6471cb953e05f03a986691a7559a05e667
RookMG <tngks1995@naver.com>
2023-01-14 14:14:55 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,19 @@
+def mat_mul(A,B):
+    C = [[0]*len(A) for _ in range(len(B[0]))]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            for k in range(len(B[0])):
+                C[i][k] = C[i][k]+(A[i][j]*B[j][k])
+    return C
+def mat_pow(A,num):
+    if num==1:
+        return A
+    elif num%2==1:
+        return mat_mul(mat_pow(A,num-1),A)
+    else:
+        return mat_pow(mat_mul(A,A),num//2)
+n = int(input())
+if n<2:
+    print(n)
+else:
+    print(mat_pow([[1,1],[1,0]],n-1)[0][0])
