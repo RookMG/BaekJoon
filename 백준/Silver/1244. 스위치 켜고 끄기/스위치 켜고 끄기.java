4a0342632862b93af73fbe912818aa65782b0f61
RookMG <tngks1995@naver.com>
2023-01-20 10:34:39 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 76 ms, Memory: 11600 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public void solution() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] sw = new int[n+1];
+    	st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++) {
+        	sw[i] = Integer.parseInt(st.nextToken());
+        }
+        int order = Integer.parseInt(br.readLine());
+        for(int i=0;i<order;i++) {
+        	st = new StringTokenizer(br.readLine());
+        	if(Integer.parseInt(st.nextToken())==1) {
+        		int num = Integer.parseInt(st.nextToken());
+        		for(int j = num;j<=n;j+=num) {
+        			sw[j] = sw[j]==1?0:1;
+        		}
+        	}else {
+        		int num = Integer.parseInt(st.nextToken());
+        		sw[num] = sw[num]==1?0:1;
+        		for(int j=1;num-j>0&&num+j<=n&&sw[num-j]==sw[num+j];j++) {
+        			sw[num-j] = sw[num+j] = sw[num+j]==1?0:1;
+        		}
+        	}
+        }
+        for(int i=1;i<=n;i++) {
+        	sb.append(sw[i]).append(i%20==0?"\n":" ");
+        }
+        System.out.print(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
