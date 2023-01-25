21bb7fa9a8899ec459cabb7919f7736d77abe4b2
RookMG <tngks1995@naver.com>
2023-01-25 14:09:36 +0900

    [Bronze I] Title: 비밀편지, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/2596. 비밀편지/비밀편지.java
+++ 백준/Bronze/2596. 비밀편지/비밀편지.java
@@ -3,28 +3,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static String[] codeTable = {"000000","001111","010011","011100","100110","101001","110101","111010"};
+	static char[][] codeTable;
+	static void initTable() {
+		codeTable = new char[8][6];
+		codeTable[0]="000000".toCharArray();
+		codeTable[1]="001111".toCharArray();
+		codeTable[2]="010011".toCharArray();
+		codeTable[3]="011100".toCharArray();
+		codeTable[4]="100110".toCharArray();
+		codeTable[5]="101001".toCharArray();
+		codeTable[6]="110101".toCharArray();
+		codeTable[7]="111010".toCharArray();
+	}
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        String input = br.readLine();
-        
-        decode:for(int i = 0;i<n;i++) {
-            String sub = input.substring(i*6, (i+1)*6);
-            for(int j = 0;j<8;j++) {
-                if(2>Integer.bitCount(Integer.parseInt(sub,2)^Integer.parseInt(codeTable[j],2))) {
-                    sb.append((char)('A'+j));
-                    continue decode;
-                }
-            }
-            System.out.println(i+1);
-            return;
-        }
-        
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		initTable();
+		int n = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+		for(int i = 0;i<n;i++) {
+			String result = decode(input.substring(i*6, (i+1)*6));
+			if("ABCDEFGH".indexOf(result)>=0) {
+				sb.append(result);
+			}else {
+				System.out.println(i+1);
+				return;
+			}
+		}
+		System.out.println(sb);
+	}
+	static String decode(String code) {
+		char[] arr = code.toCharArray();
+		for(int i=0;i<8;i++) {
+			int count = 0;
+			for(int j=0;j<6;j++) {
+				if(arr[j]==codeTable[i][j]) {count++;}
+			}
+			if(count>=5) {return Character.toString((char)('A'+i));}
+		}
+		return "0";
+	}
 }
