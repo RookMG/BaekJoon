848c7de43b970a79b9a8525e78af60c91ae06c59
RookMG <tngks1995@naver.com>
2023-01-30 17:41:23 +0900

    [Silver II] Title: 이항 계수 2, Time: 280 ms, Memory: 130112 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,9 @@
+from fractions import Fraction
+n, m = map(int,input().split())
+answer = 1
+div = 1
+for i in range(n-m+1,n+1):
+    answer*=i
+for i in range(1,1+m):
+    div*=i
+print(Fraction(answer,div)%10007)
