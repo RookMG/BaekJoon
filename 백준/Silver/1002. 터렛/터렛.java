6e23be49a1e0a08472751307b92243734a89275c
RookMG <tngks1995@naver.com>
2023-02-09 14:26:43 +0900

    [Silver III] Title: 터렛, Time: 84 ms, Memory: 11728 KB -BaekjoonHub

---
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception{
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken()), y1 = Integer.parseInt(st.nextToken()), r1 = Integer.parseInt(st.nextToken()), x2 = Integer.parseInt(st.nextToken()), y2 = Integer.parseInt(st.nextToken()), r2 = Integer.parseInt(st.nextToken());
+        	int dPow = (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2);
+        	if(dPow==0&&r1==r2) {
+        		sb.append(-1).append("\n");
+        	}
+        	else if(dPow>(r1+r2)*(r1+r2) || (dPow<(r1-r2)*(r1-r2))) {
+        		sb.append(0).append("\n");
+        	}
+        	else if(dPow==(r1+r2)*(r1+r2) || (dPow==(r1-r2)*(r1-r2))) {
+        		sb.append(1).append("\n");
+        	}
+        	else {
+        		sb.append(2).append("\n");
+        	}
+        }
+        System.out.print(sb);
+    }
+}
