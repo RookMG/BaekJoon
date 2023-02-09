c9e322f66f315c6924ee7f5454d9ec293c3e351a
RookMG <tngks1995@naver.com>
2023-02-09 10:57:39 +0900

    [Silver V] Title: 카드1, Time: 92 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine()), k = 2, position = -1;
+        LinkedList<Integer> circle = new LinkedList<Integer>();
+        circle.add(n);
+        for(int i=1;i<n;i++){
+            circle.add(i);
+        }
+        while(n!=0){
+            position=(position+k)%n;
+            sb.append(circle.get(position));
+            circle.remove(position);
+            position--;
+            if(n!=1)
+                sb.append(" ");
+            n--;
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception{
+        new Main().solution();
+    }
+}
