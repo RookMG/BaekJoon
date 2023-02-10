b855947782e23510250d03ef9f33d76da34cd03c
RookMG <tngks1995@naver.com>
2023-02-10 10:38:54 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 4352 ms, Memory: 665324 KB -BaekjoonHub

---
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int R,C,K;
+    static int[][] delta = {{-1,0},{1,0},{0,-1},{0,1}};
+    static char[][] map;
+    static boolean[][][][] visit;
+    
+    static class Status{
+    	int r, c, time, wall;
+		public Status(int r, int c, int time, int wall) {
+			this.r = r;
+			this.c = c;
+			this.time = time;
+			this.wall = wall;
+		}
+    }
+    
+    public static void main(String[] args) throws Exception{
+    	st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken()); C = Integer.parseInt(st.nextToken()); K = Integer.parseInt(st.nextToken());
+        map = new char[R][C]; visit = new boolean[R][C][K+1][2];
+        for(int i=0;i<R;i++){
+            map[i] = br.readLine().toCharArray();
+        }
+        
+        Queue<Status> q = new ArrayDeque();
+        q.add(new Status(0,0,1,K));
+        
+        while(!q.isEmpty()){
+        	Status status = q.poll();
+            if (status.r == R - 1 && status.c == C - 1) {
+                System.out.println(status.time);
+                return;
+            }
+            for (int[] d : delta) {
+                int nr = status.r + d[0], nc = status.c + d[1];
+                if (isIn(nr, nc)) {
+                    if (map[nr][nc] == '0' && !visit[nr][nc][status.wall][status.time%2]) {
+                        visit[nr][nc][status.wall][status.time%2] = true;
+                        q.add(new Status(nr, nc, status.time + 1, status.wall));
+                    } else if (map[nr][nc] == '1' && status.time%2==1 && status.wall!=0 && !visit[nr][nc][status.wall-1][status.time%2]) {
+                        visit[nr][nc][status.wall-1][status.time%2] = true;
+                        q.add(new Status(nr, nc, status.time + 1, status.wall - 1));
+                    }
+                }
+            }
+            if (!visit[status.r][status.c][status.wall][status.time%2]) {
+            	visit[status.r][status.c][status.wall][status.time%2] = true;
+            	status.time++;
+            	q.add(status);
+            }
+        }
+        
+        System.out.println(-1);
+    }
+
+    static boolean isIn(int r, int c){
+        return 0<=r&&r<R&&0<=c&&c<C;
+    }
+}
