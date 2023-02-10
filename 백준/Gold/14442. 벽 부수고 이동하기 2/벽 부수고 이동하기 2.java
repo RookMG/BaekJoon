1db6f221c8630d5ba883f02091574777c412cf71
RookMG <tngks1995@naver.com>
2023-02-10 10:39:08 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1504 ms, Memory: 334496 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int R,C,K, answer;
+    static int R,C,K;
     static int[][] delta = {{-1,0},{1,0},{0,-1},{0,1}};
     static char[][] map;
     static boolean[][][] visit;
@@ -24,19 +24,20 @@ public class Main{
     
     public static void main(String[] args) throws Exception{
     	st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken()); C = Integer.parseInt(st.nextToken()); K = Integer.parseInt(st.nextToken()); answer = R*C+1;
+        R = Integer.parseInt(st.nextToken()); C = Integer.parseInt(st.nextToken()); K = Integer.parseInt(st.nextToken());
         map = new char[R][C]; visit = new boolean[R][C][K+1];
         for(int i=0;i<R;i++){
             map[i] = br.readLine().toCharArray();
         }
         
-        Queue<Status> q = new LinkedList<>();
+        Queue<Status> q = new ArrayDeque<>();
         q.add(new Status(0,0,1,K));
         
         while(!q.isEmpty()){
         	Status status = q.poll();
             if (status.r == R - 1 && status.c == C - 1) {
-                answer = Math.min(answer, status.time);
+                System.out.println(status.time);
+                return;
             }
             for (int[] d : delta) {
                 int nr = status.r + d[0], nc = status.c + d[1];
@@ -52,7 +53,7 @@ public class Main{
             }
         }
         
-        System.out.println(answer!=R*C+1?answer:-1);
+        System.out.println(-1);
     }
 
     static boolean isIn(int r, int c){
