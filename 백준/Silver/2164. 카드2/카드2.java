31adc00023909b3716acf097a804f3ca9929c634
RookMG <tngks1995@naver.com>
2023-02-11 19:26:23 +0900

    [Silver IV] Title: 카드2, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -2,13 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public void solution() throws Exception {
-        int n = Integer.parseInt(br.readLine()), answer = 1;
-        for (int i = 2; i<=n; i++)
-            answer = (answer + 1) % i + 1;
-        System.out.println(answer==1?n:answer-1);
-    }
+    static int n, i;
     public static void main(String[] args) throws Exception{
-        new Main().solution();
+        if((n = Integer.parseInt(br.readLine()))<2){
+            System.out.println(n); return;
+        }
+        for(i=1;i<n;i<<=1);
+        System.out.println((n-(i>>1))<<1);
     }
 }
