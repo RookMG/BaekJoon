4802a5e0ad09e0b69be5b2ecdfd39a49d88e238c
RookMG <tngks1995@naver.com>
2023-02-13 10:46:04 +0900

    [Platinum IV] Title: 선분 교차 3, Time: 80 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Platinum/20149. 선분 교차 3/선분 교차 3.java
+++ 백준/Platinum/20149. 선분 교차 3/선분 교차 3.java
@@ -6,52 +6,52 @@ public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    class Point{
+    static boolean crossing;
+    class Point implements Comparable<Point>{
         public long x, y;
         public Point(int x, int y){
             this.x = x;
             this.y = y;
         }
+		@Override
+		public int compareTo(Point o) {
+			// TODO Auto-generated method stub
+			return x==o.x?Long.compare(y, o.y):Long.compare(x, o.x);
+		}
     }
     static int outPdctDirection(Point p1, Point p2, Point p3){
         long result = (p3.x-p1.x)*(p1.y-p2.y)-(p1.x-p2.x)*(p3.y-p1.y);
-        return result>0?1:(result<0?-1:0);
+        return Long.compare(result, 0);
     }
-    static double[] findCross(Point p1, Point p2, Point p3, Point p4){
-        double[] cross = new double[2];
-        cross[0] = 1.0*((p1.x*p2.y-p1.y*p2.x)*(p3.x-p4.x)-(p3.x*p4.y-p3.y*p4.x)*(p1.x-p2.x))/((p1.x-p2.x)*(p3.y-p4.y)-(p1.y-p2.y)*(p3.x-p4.x));
-        cross[1] = 1.0*((p1.x*p2.y-p1.y*p2.x)*(p3.y-p4.y)-(p3.x*p4.y-p3.y*p4.x)*(p1.y-p2.y))/((p1.x-p2.x)*(p3.y-p4.y)-(p1.y-p2.y)*(p3.x-p4.x));
-        return cross;
+    static void printCross(Point p1, Point p2, Point p3, Point p4){
+    	double div = (p1.x-p2.x)*(p3.y-p4.y)-(p1.y-p2.y)*(p3.x-p4.x);
+    	if(crossing) {
+    		sb.append(1).append("\n");
+	        if(div!=0) {
+	        	sb.append(1.0*((p1.x*p2.y-p1.y*p2.x)*(p3.x-p4.x)-(p3.x*p4.y-p3.y*p4.x)*(p1.x-p2.x))/div).append(" ").append(1.0*((p1.x*p2.y-p1.y*p2.x)*(p3.y-p4.y)-(p3.x*p4.y-p3.y*p4.x)*(p1.y-p2.y))/div).append("\n");
+	        }else if(p1.compareTo(p4)==0&&p3.compareTo(p1)<=0){
+	        	sb.append(p1.x).append(" ").append(p1.y).append("\n");
+	        }else if(p2.compareTo(p3)==0&&p1.compareTo(p3)<=0){
+	        	sb.append(p2.x).append(" ").append(p2.y).append("\n");
+	        }
+    	}else {
+    		sb.append(0).append("\n");
+    	}
     }
     public void solution() throws Exception {
         st = new StringTokenizer(br.readLine());
-        Point p1 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())),
+        Point 	p1 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())),
                 p2 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
         st = new StringTokenizer(br.readLine());
-        Point p3 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())),
+        Point 	p3 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())),
                 p4 = new Point(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        Point tmp;
+        if(p1.compareTo(p2)>0) {tmp=p1;p1=p2;p2=tmp;}
+        if(p3.compareTo(p4)>0) {tmp=p3;p3=p4;p4=tmp;}
         int product1 = outPdctDirection(p1,p2,p3)*outPdctDirection(p1,p2,p4), product2 = outPdctDirection(p3,p4,p1)*outPdctDirection(p3,p4,p2);
-        if(product1<=0&&product2<=0) {
-            if (product1 == 0 && product2 == 0 && !(Math.min(p1.x, p2.x) <= Math.max(p3.x, p4.x) && Math.min(p3.x, p4.x) <= Math.max(p1.x, p2.x)) || !(Math.min(p1.y, p2.y) <= Math.max(p3.y, p4.y) && Math.min(p3.y, p4.y) <= Math.max(p1.y, p2.y)))
-                sb.append(0).append("\n");
-            else{
-                sb.append(1).append("\n");
-                if(!(product1 == 0 && product2 == 0)){
-                    double[] cross = findCross(p1,p2,p3,p4);
-                    sb.append(cross[0]).append(" ").append(cross[1]).append("\n");
-                }
-                else if(Math.min(p3.x,p4.x)==Math.max(p1.x,p2.x)&&Math.min(p3.y,p4.y)==Math.max(p1.y,p2.y)){
-                    sb.append(Math.min(p3.x,p4.x)).append(" ").append(Math.min(p3.y,p4.y)).append("\n");
-                }else if(Math.min(p1.x,p2.x)==Math.max(p3.x,p4.x)&&Math.min(p1.y,p2.y)==Math.max(p3.y,p4.y)){
-                    sb.append(Math.min(p1.x,p2.x)).append(" ").append(Math.min(p1.y,p2.y)).append("\n");
-                }else if((p2.x-p1.x)*(p4.y-p3.y)!=(p2.y-p1.y)*(p4.x-p3.x)){
-                    double[] cross = findCross(p1,p2,p3,p4);
-                    sb.append(cross[0]).append(" ").append(cross[1]).append("\n");
-                }
-            }
-        }
-        else
-            sb.append(0).append("\n");
+        if(product1==0&&product2==0) {crossing = p1.compareTo(p4)<=0&&p2.compareTo(p3)>=0;}
+        else {crossing = product1<=0&&product2<=0;}
+        printCross(p1,p2,p3,p4);
         System.out.print(sb);
     }
     public static void main(String[] args) throws Exception{
