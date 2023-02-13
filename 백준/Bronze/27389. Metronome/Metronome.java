840cb5b4df402ec450abfe733d3a4c16cb3bff1f
RookMG <tngks1995@naver.com>
2023-02-13 10:49:43 +0900

    [Bronze V] Title: Metronome, Time: 84 ms, Memory: 11596 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception{
+        System.out.print(Double.parseDouble(br.readLine())/4);
+    }
+}
