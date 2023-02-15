de695f47c12adf503cdee6fd9ebd1cb90f7a4b69
RookMG <tngks1995@naver.com>
2023-02-15 16:56:19 +0900

    [Silver V] Title: 색종이, Time: 84 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static boolean[][] board = new boolean[101][101];
+    public static void main(String[] args) throws Exception{
+        int n = Integer.parseInt(br.readLine()), answer = 0;
+        for(int i=0;i<n;i++) {
+	        st = new StringTokenizer(br.readLine());
+	        int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+	        for(int r=0;r<10;r++) {
+	        	for(int c=0;c<10;c++) {
+	        		board[x+r][y+c] = true;
+	        	}
+	        }
+        }
+        for(int r=0;r<100;r++) {
+        	for(int c=0;c<100;c++) {
+        		if(board[r][c]) answer++;
+        	}
+        }
+        System.out.print(answer);
+    }
+}
