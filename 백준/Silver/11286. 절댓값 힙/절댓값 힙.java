d65144e5c19a8badea7577153db876a6ad155d42
RookMG <tngks1995@naver.com>
2023-02-15 12:02:08 +0900

    [Silver I] Title: 절댓값 힙, Time: 296 ms, Memory: 24128 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,15 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     public static void main(String[] args) throws Exception{
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1,o2)->Math.abs(o1)==Math.abs(o2)?o1-o2:Math.abs(o1)-Math.abs(o2));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (Math.abs(o1)==Math.abs(o2)) {
+					return Integer.compare(o1, o2);
+				}else return Integer.compare(Math.abs(o1),Math.abs(o2));
+			}
+		});
         for(int i=0;i<n;i++) {
         	int num = Integer.parseInt(br.readLine());
         	if(num==0) {
