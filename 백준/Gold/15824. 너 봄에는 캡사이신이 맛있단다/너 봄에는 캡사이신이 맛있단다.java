86881c194fa6ca4910c74ba77dc09dc9c9aa63c4
RookMG <tngks1995@naver.com>
2023-02-16 09:04:25 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 156 ms, Memory: 13656 KB -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
@@ -4,30 +4,31 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main{
-    static final int MOD = 1000000007;
+    static final long MOD = 1000000007L;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static int[] pows;
     public static void main(String[] args) throws Exception{
         int n = Integer.parseInt(br.readLine());
         long answer = 0;
         int[] arr = new int[n];
+        pows = new int[n+1];
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                answer+=(arr[j]-arr[i])*pow(2,j-i-1);
-                answer%=MOD;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                answer+=((long)(arr[j]-arr[i]))*pow(j-i-1)%MOD;
             }
         }
-        System.out.print(answer);
+        System.out.print(answer%MOD);
     }
-    static long pow(int n, int k){
-        if(k==0) return 1;
-        else if(k==1) return n;
-        long prev = pow(n,k/2);
-        return k%2==0?prev*prev%MOD:prev*prev*n%MOD;
+    static long pow(int k){
+        if(k<2) return k+1;
+        if(pows[k]!=0) return pows[k];
+        long prev = pow(k/2);
+        return pows[k] = (int)(k%2==0?(prev*prev)%MOD:(prev*prev*2)%MOD);
     }
 }
