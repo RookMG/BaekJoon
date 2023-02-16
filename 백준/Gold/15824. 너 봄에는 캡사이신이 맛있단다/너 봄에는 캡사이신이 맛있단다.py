d77808721a6d5aae6739c8985d5fbbf3ce499950
RookMG <tngks1995@naver.com>
2023-02-16 09:17:53 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 460 ms, Memory: 116088 KB -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,8 +1,17 @@
-MOD = 1000000007
 n, answer = int(input()), 0
 arr = list(map(int,input().split()))
 arr.sort()
+pows = [0 for _ in range(n+1)]
+pows[0] = 1
+pows[1] = 2
+
+def square(k):
+    if pows[k]==0:
+        pows[k] = 2 * (square(k // 2)**2) % 1000000007 if k % 2 == 1 else (square(k // 2)**2)
+    return pows[k]
+
+
 for i in range(len(arr)-1):
-    for j in range(i,len(arr)):
-        answer +=(arr[j]-arr[i])*pow(2,j-i-1,MOD)%MOD
-print(answer%MOD)
+    for j in range(i+1,len(arr)):
+        answer +=(arr[j]-arr[i])*square(j-i-1)%1000000007
+print(answer%1000000007)
