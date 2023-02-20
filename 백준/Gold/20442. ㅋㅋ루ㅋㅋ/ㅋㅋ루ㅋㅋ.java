d08ed88287ea1b32b4b57c8ee3ab0e71c110c461
RookMG <tngks1995@naver.com>
2023-02-21 08:33:05 +0900

    [Gold II] Title: ㅋㅋ루ㅋㅋ, Time: 252 ms, Memory: 43024 KB -BaekjoonHub

---
+++ 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+		char[] line = br.readLine().toCharArray();
+		int answer = 0, r = 0, k = 0, left = 0, right = line.length-1, lk =0, rk = 0;
+		for(char c:line) {
+			if(c=='R') r++;
+			else k++;
+		}
+		while(left<=right) {
+			if(lk<rk) {
+				if(line[left++]=='K') lk++;
+				else answer = Math.max(answer, r-- + 2*Math.min(lk, rk));
+			}
+			else {
+				if(line[right--]=='K') rk++;
+				else answer = Math.max(answer, r-- + 2*Math.min(lk, rk));
+			}
+		}
+		bw.write(Integer.toString(answer));
+		bw.flush();
+	}
+}
