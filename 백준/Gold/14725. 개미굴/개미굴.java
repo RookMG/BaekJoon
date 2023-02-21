edb95033cfbd9c411a3ea120d942b7c78836cbe2
RookMG <tngks1995@naver.com>
2023-02-21 13:17:57 +0900

    [Gold III] Title: 개미굴, Time: 120 ms, Memory: 13164 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static class Node implements Comparable<Node>{
+		ArrayList<Node> child;
+		String name;
+		Node(String name) {
+			child = new ArrayList<>();
+			this.name = name;
+		}
+		void sort() {
+			Collections.sort(child);
+			for(Node n:child) n.sort();
+		}
+		Node getByName(String name) {
+			for(Node n:child) {
+				if(n.name.equals(name)) {
+					return n;
+				}
+			}
+			Node n = new Node(name);
+			child.add(n);
+			return n;
+		}
+		void print(int depth) throws Exception {
+			for(int i=0;i<depth;i++) {
+				bw.write("--");
+			}
+			bw.write(name);
+			bw.write('\n');
+			for(Node n:child) {
+				n.print(depth+1);
+			}
+		}
+		@Override
+		public int compareTo(Node o) {
+			return name.compareTo(o.name);
+		}
+	}
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		Node top = new Node(""), tmp;
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int l = Integer.parseInt(st.nextToken());
+			tmp = top;
+			for(int j=0;j<l;j++) {
+				tmp = tmp.getByName(st.nextToken());
+			}
+		}
+		top.sort();
+		for(Node node:top.child)
+			node.print(0);
+		bw.flush();
+	}
+}
