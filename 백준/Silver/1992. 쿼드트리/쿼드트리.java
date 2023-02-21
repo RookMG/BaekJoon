9dca903764a994d6f81e19d8cf851858c1c7b674
RookMG <tngks1995@naver.com>
2023-02-22 08:14:51 +0900

    [Silver I] Title: 쿼드트리, Time: 80 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -5,13 +5,15 @@ import java.io.OutputStreamWriter;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
 	static int n;
 	static char[][] img;
 	public static void main(String[] args) throws Exception {
 		n = Integer.parseInt(br.readLine());
 		img = new char[n][n];
 		for(int i=0;i<n;i++) img[i] = br.readLine().toCharArray();
-		bw.write(zipImg(n,0,0,n,n));
+		zipImg(n,0,0,n,n);
+		bw.write(sb.toString());
 		bw.flush();
 	}
 	static boolean oneColor(int sr,int sc, int er, int ec) {
@@ -21,11 +23,17 @@ public class Main {
 				if(now!=img[i][j]) return false;
 		return true;
 	}
-	static String zipImg(int size, int sr, int sc, int er, int ec) {
-		if(size==1||oneColor(sr,sc,er,ec)) return String.valueOf(img[sr][sc]);
-		StringBuilder sb = new StringBuilder();
+	static void zipImg(int size, int sr, int sc, int er, int ec) {
+		if(size==1||oneColor(sr,sc,er,ec)) {
+			sb.append(img[sr][sc]);
+			return;
+		}
 		size/=2;
-		sb.append("(").append(zipImg(size,sr,sc,er-size,ec-size)).append(zipImg(size,sr,sc+size,er-size,ec)).append(zipImg(size,sr+size,sc,er,ec-size)).append(zipImg(size,sr+size,sc+size,er,ec)).append(")");
-		return sb.toString();
+		sb.append("(");
+		zipImg(size,sr,sc,er-size,ec-size);
+		zipImg(size,sr,sc+size,er-size,ec);
+		zipImg(size,sr+size,sc,er,ec-size);
+		zipImg(size,sr+size,sc+size,er,ec);
+		sb.append(")");
 	}
 }
