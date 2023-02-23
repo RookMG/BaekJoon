5bcb12ffcffc1d7a4f9517883627096909b907d4
RookMG <tngks1995@naver.com>
2023-02-23 20:54:05 +0900

    [Gold IV] Title: 알파벳, Time: 140 ms, Memory: 12320 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,32 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	static int R, C, answer = 0;
 	static char[][] map;
 	static final int[][] delta = {{-1,0},{1,0},{0,-1},{0,1}};
-	static long[] check = new long[26];
+	static int[][] visit;
 	public static void main(String[] args) throws Exception {
 		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken()); C = Integer.parseInt(st.nextToken());
-		map = new char[R][C];
-		long l = 1L;
-		for(int i=0;i<26;i++,l<<=1)check[i] = l;
+		map = new char[R][C]; visit = new int[R][C];
 		for(int i=0;i<R;i++) map[i] = br.readLine().toCharArray();
-		recur(0,0,1,1L<<(map[0][0]-'A'));
-		System.out.print(answer);
+		visit[0][0] = 1<<(map[0][0]-'A');
+		recur(0,0,1,1<<(map[0][0]-'A'));
+		bw.write(Integer.toString(answer));
+		bw.flush();
 	}
-	static void recur(int r, int c, int l, long visit){
+	static void recur(int r, int c, int l, int bits){
 		for(int i=0;i<4;i++){
 			int nr = r + delta[i][0], nc = c + delta[i][1];
 			if(isIn(nr,nc)){
-				long check = 1L<<(map[nr][nc]-'A');
-				if((visit&check)!=0L) answer = Math.max(answer,l);
-				else{
-					recur(nr,nc,l+1,visit|check);
+				int check = 1<<(map[nr][nc]-'A'), next = bits|check;
+				if((bits&check)!=0) answer = Math.max(answer,l);
+				else if(visit[nr][nc]!=next){
+					visit[nr][nc] = next;
+					recur(nr,nc,l+1,next);
 				}
 			}
 		}
