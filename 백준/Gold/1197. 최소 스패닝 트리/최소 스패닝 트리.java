32f216b1eb7a5cd439d1549267c19e6a124e4b1f
RookMG <tngks1995@naver.com>
2023-02-28 15:01:23 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 528 ms, Memory: 56260 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,41 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main{
-	static int[] parent;
-	static int findParent(int i) {
-		return parent[i]==i?parent[i]:(parent[i]=findParent(parent[i]));
+class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static class Node implements Comparable<Node>{
+		int num;
+		long value;
+		public Node(int num, long value) {
+			this.num = num;
+			this.value = value;
+		}
+		@Override
+		public int compareTo(Node o) {
+			return Long.compare(value,o.value);
+		}
 	}
-    public void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken()), answer = 0;
-        parent = new int[v+1];
-        for(int i=0;i<=v;i++) {
-        	parent[i]=i;
-        }
-        int[][] links = new int[e][3];
-        for(int i=0;i<e;i++) {
-        	st = new StringTokenizer(br.readLine());
-        	for(int j=0;j<3;j++) {
-        		links[i][j]=Integer.parseInt(st.nextToken());
-        	}
-        }
-        Arrays.sort(links,(e1,e2) ->{
-        	return e1[2]-e2[2];
-        });
-        for(int[] l:links) {
-        	int ap = findParent(l[0]), bp = findParent(l[1]);
-        	if(ap!=bp) {
-        		answer+=l[2];
-        		parent[Math.max(ap,bp)]=Math.min(ap,bp);
-        	}
-        }
-        System.out.println(answer);
-    }
-    public static void main(String[] args) throws Exception{
-        new Main().solution();
-    }
+	static class Links{
+		ArrayList<int[]> links = new ArrayList<>();
+	}
+	public static void main(String[] args) throws Exception{
+		st = new StringTokenizer(br.readLine());
+		int v = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken()), cnt = 0;
+		long answer = 0;
+		boolean[] visit = new boolean[v+1];
+		long[] costs = new long[v+1];
+		Links[] l = new Links[v+1];
+		for(int i=1;i<=v;i++) l[i] = new Links();
+		for(int i=0;i<e;i++){
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+			l[a].links.add(new int[]{b,c});
+			l[b].links.add(new int[]{a,c});
+		}
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		Arrays.fill(costs, Long.MAX_VALUE);
+		pq.offer(new Node(1,0));
+		while(!pq.isEmpty()) {
+			Node head = pq.poll();
+			if(visit[head.num]) continue;
+			visit[head.num] = true;
+			answer+=head.value;
+			if(++cnt == v) break;
+			for(int[] link : l[head.num].links){
+				if(!visit[link[0]] && costs[link[0]] > link[1]) {
+					costs[link[0]] = link[1];
+					pq.offer(new Node(link[0], costs[link[0]]));
+				}
+			}
+		}
+		bw.write(Long.toString(answer));
+		bw.write("\n");
+		bw.flush();
+	}
 }
