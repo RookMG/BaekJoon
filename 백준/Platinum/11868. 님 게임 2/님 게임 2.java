5eec8f687d816d8ef634750cf43f88b5f9e22cce
RookMG <tngks1995@naver.com>
2023-03-15 08:24:26 +0900

    [Platinum IV] Title: 님 게임 2, Time: 80 ms, Memory: 11896 KB -BaekjoonHub

--- 백준/Platinum/11868. 님 게임 2/님 게임 2.java
+++ 백준/Platinum/11868. 님 게임 2/님 게임 2.java
@@ -1,17 +1,14 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    public static void main(String[] args) throws Exception {
-        int n = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int grundy = Integer.parseInt(st.nextToken());
-        for(int i=1;i<n;i++){
-            grundy^=Integer.parseInt(st.nextToken());
-        }
-        bw.write(grundy==0?"cubelover":"koosaga");
-        bw.flush();
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine()), g = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) g^=Integer.parseInt(st.nextToken());
+		bw.write(g==0?"cubelover":"koosaga");
+		bw.flush();
+	}
 }
