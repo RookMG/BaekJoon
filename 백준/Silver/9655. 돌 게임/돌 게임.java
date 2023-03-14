b27173ea97ea4cc721932bdf3fb18efb6cca3560
RookMG <tngks1995@naver.com>
2023-03-14 20:49:41 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        bw.write(n%2==0?"CY":"SK");
+        bw.flush();
+    }
+}
