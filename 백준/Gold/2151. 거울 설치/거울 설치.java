f3cf74aa38724d80a0ba5707cef4b993b3623e77
RookMG <tngks1995@naver.com>
2023-03-23 10:05:28 +0900

    [Gold III] Title: 거울 설치, Time: 160 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.java
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -6,10 +6,12 @@ public class Main {
     static final int[][] delta = {{-1,0},{0,-1},{1,0},{0,1}}; //↑←↓→
     static int N, answer = Integer.MAX_VALUE;
     static char[][] map;
+    static int[][][] visit;
     public static void main(String[] args) throws Exception {
         N = Integer.parseInt(br.readLine());
         map = new char[N][];
         for(int i=0;i<N;i++) map[i] = br.readLine().toCharArray();
+        visit = new int[N][N][4];
         for(int i=0;i<N;i++) {
             for(int j=0;j<N;j++){
                 if(map[i][j]=='#'){
@@ -25,7 +27,9 @@ public class Main {
         bw.flush();
     }
     static void dfs(int r, int c, int dir, int cost){
+        if(visit[r][c][dir]!=0&&visit[r][c][dir]<=cost) return;
         if(cost>=answer) return;
+        visit[r][c][dir] = cost+1;
         while(isIn(r+=delta[dir][0],c+=delta[dir][1])){
             switch (map[r][c]){
                 case '#':
