f3e31d6331921c47c2a1ca48fec50533a4fdffdf
RookMG <tngks1995@naver.com>
2023-03-23 09:59:16 +0900

    [Gold III] Title: 거울 설치, Time: 1796 ms, Memory: 15260 KB -BaekjoonHub

---
+++ 백준/Gold/2151. 거울 설치/거울 설치.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final int[][] delta = {{-1,0},{0,-1},{1,0},{0,1}}; //↑←↓→
+    static int N, answer = Integer.MAX_VALUE;
+    static char[][] map;
+    static int[][][] visit;
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][];
+        for(int i=0;i<N;i++) map[i] = br.readLine().toCharArray();
+        visit = new int[N][N][4];
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++){
+                if(map[i][j]=='#'){
+                    map[i][j]='*';
+                    for(int d=0;d<4;d++){
+                        dfs(i,j,d,0);
+                    }
+                    break;
+                }
+            }
+        }
+        bw.write(Integer.toString(answer));
+        bw.flush();
+    }
+    static void dfs(int r, int c, int dir, int cost){
+        if(visit[r][c][dir]!=0&&visit[r][c][dir]<=cost) return;
+        if(cost>answer) return;
+        visit[r][c][dir] = cost+1;
+        while(isIn(r+=delta[dir][0],c+=delta[dir][1])){
+            switch (map[r][c]){
+                case '#':
+                    answer = Math.min(answer,cost);
+                case '*':
+                    return;
+                case '!':
+                    dfs(r,c,(dir+1)%4,cost+1);
+                    dfs(r,c,(dir+3)%4,cost+1);
+                default:
+            }
+        }
+    }
+    static boolean isIn(int nr, int nc){
+        return 0<=nr&&nr<N&&0<=nc&&nc<N;
+    }
+}
