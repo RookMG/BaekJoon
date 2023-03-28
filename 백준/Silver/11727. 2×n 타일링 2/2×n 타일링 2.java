a71c54c5386ea5c749ca70c5f0a95518290c0f96
RookMG <tngks1995@naver.com>
2023-03-28 12:35:49 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,18 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-public class Main{
+import java.io.*;
+public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public void solution() throws Exception {
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[Math.max(3,n+1)];
-        dp[1] = 1;
-        dp[2] = 3;
-        for(int i=3;i<=n;i++){
-            dp[i] = (dp[i-2]*2 + dp[i-1])%10007;
-        }
-        System.out.println(dp[n]);
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final int MOD = 10007;
+    static int[][] mat = {{1,2},{1,0}};
+    public static void main(String[] args) throws Exception {
+        bw.write(Integer.toString(matPow(mat,Integer.parseInt(br.readLine()))[0][0]%MOD));
+        bw.flush();
     }
-    public static void main(String[] args) throws Exception{
-        new Main().solution();
+    static int[][] matMul(int[][] a, int[][] b){
+        int[][] c = new int[b[0].length][a.length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                for(int k=0;k<b[0].length;k++){
+                    c[i][k] = (c[i][k]+(a[i][j]*b[j][k])%MOD)%MOD;
+                }
+            }
+        }
+        return c;
+    }
+    static int[][] matPow(int[][] a, int num){
+        if(num==1) return a;
+        return num%2==1?matMul(matPow(a,num-1),a):matPow(matMul(a,a),num/2);
     }
 }
