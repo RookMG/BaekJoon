d456a1fb074a807b62c67bf63b3a13e3d42ea42d
RookMG <tngks1995@naver.com>
2023-03-28 15:32:59 +0900

    [Gold III] Title: 1로 만들기 3, Time: 132 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Gold/27440. 1로 만들기 3/1로 만들기 3.java
+++ 백준/Gold/27440. 1로 만들기 3/1로 만들기 3.java
@@ -3,33 +3,30 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
+    static HashSet<Long> visit;
+    static ArrayDeque<Long> q;
     public static void main(String[] args) throws Exception{
         long n = Long.parseLong(br.readLine()), now = 0, answer = -1;
-        HashSet<Long> visit = new HashSet<>();
-        ArrayDeque<Long> q = new ArrayDeque<>();
+        visit = new HashSet<>();
+        q = new ArrayDeque<>();
         q.offer(n);
         while(now!=1){
             for(int i=q.size();i>0;i--){
                 now = q.pollFirst();
                 if(now==1)
                     break;
-                if(now%2==0&&!visit.contains(now/2)){
-                    visit.add(now/2);
-                    q.offerLast(now/2);
-                }
-                if(now%3==0&&!visit.contains(now/3)){
-                    visit.add(now/3);
-                    q.offerLast(now/3);
-                }
-                if(!visit.contains(now-1)){
-                    visit.add(now-1);
-                    q.offerLast(now-1);
-                }
+                if(now%3==0) next(now/3);
+                if(now%2==0) next(now/2);
+                next(now-1);
             }
             answer++;
         }
         bw.write(Long.toString(answer));
         bw.flush();
+    }
+    static void next(long num){
+        if(visit.contains(num)) return;
+        visit.add(num);
+        q.offerLast(num);
     }
 }
