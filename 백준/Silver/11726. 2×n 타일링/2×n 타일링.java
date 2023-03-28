1002155f9be301cf13a105ade4291ca518ea9619
RookMG <tngks1995@naver.com>
2023-03-28 12:22:07 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,22 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class Main{
-
-    public void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception{
         int n = Integer.parseInt(br.readLine());
         int[] fill = new int[n];
         fill[0]=1;
-        if(n!=1){
-            fill[1]=2;
-        }
-        for(int i=2;i<n;i++){
-            fill[i]=(fill[i-2]+fill[i-1])%10007;
-        }
-        System.out.println(fill[n-1]%10007);
-    }
-    public static void main(String[] args) throws Exception{
-        new Main().solution();
+        if(n!=1) fill[1]=2;
+        for(int i=2;i<n;i++) fill[i]=(fill[i-2]+fill[i-1])%10007;
+        bw.write(Integer.toString(fill[n-1]%10007));
+        bw.flush();
     }
 }
