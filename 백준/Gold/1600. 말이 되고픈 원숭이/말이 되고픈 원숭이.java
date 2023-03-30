fb15fcca6d674bf91ec4e84ba0448861dca4bfaa
RookMG <tngks1995@naver.com>
2023-03-30 11:20:01 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 504 ms, Memory: 58532 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,63 +1,58 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static final int[][] delta = {{-1,0},{1,0},{0,1},{0,-1}};
+	static final int[][] knight = {{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,1},{-2,-1}};
+	static int N, M, K;
 
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int[][] delta = {{-1,0},{1,0},{0,1},{0,-1}};
-    static int[][] knight = {{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,1},{-2,-1}};
-    static int N, M, K;
-    public void solution() throws Exception {
-    	K = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); N = Integer.parseInt(st.nextToken());
-        int[][] map = new int[N][M];
-        boolean[][][] visit = new boolean[N][M][K+1];
-        for(int i=0;i<N;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<M;j++)
-            	map[i][j] = Integer.parseInt(st.nextToken());
-        }
-        Deque<int[]> dq = new LinkedList<>(), nq;
-        dq.offer(new int[] {0,0,0});
-        int answer = 0;
-        while(!dq.isEmpty()) {
-        	nq = new LinkedList<>();
-        	while(!dq.isEmpty()) {
-        		int[] point = dq.poll();
-        		if(point[0]==N-1&&point[1]==M-1) {
-        	        System.out.println(answer);
-        	        return; 
-        	    }
-        		for(int i=0;i<delta.length;i++) {
-        			int nr = point[0]+delta[i][0], nc = point[1]+delta[i][1];
-        			if(isIn(nr,nc)&&!visit[nr][nc][point[2]]&&map[nr][nc]==0) {
-        				visit[nr][nc][point[2]]=true;
-        				nq.add(new int[] {nr,nc,point[2]});
-        			}
-        		}
-        		if(point[2]<K) {
-        			for(int i=0;i<knight.length;i++) {
-            			int nr = point[0]+knight[i][0], nc = point[1]+knight[i][1];
-            			if(isIn(nr,nc)&&!visit[nr][nc][point[2]+1]&&map[nr][nc]==0) {
-            				visit[nr][nc][point[2]+1]=true;
-            				nq.add(new int[] {nr,nc,point[2]+1});
-            			}
-            		}
-        		}
-        	}
-        	answer++;
-        	dq = nq;
-        }
-        System.out.println(-1);
-    }
-    public static void main(String[] args) throws Exception{
-        new Main().solution();
-    }
-    static boolean isIn(int r, int c) {
-    	return 0<=r&&r<N&&0<=c&&c<M;
-    }
+	public static void main(String[] args) throws Exception{
+		K = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken()); N = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+		boolean[][][] visit = new boolean[N][M][K+1];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
+		}
+		ArrayDeque<int[]> dq = new ArrayDeque<>();
+		dq.offer(new int[] {0,0,0});
+		int answer = 0;
+		while(!dq.isEmpty()) {
+			for(int s = dq.size();s>0;s--) {
+				int[] point = dq.pollFirst();
+				if(point[0]==N-1&&point[1]==M-1) {
+					bw.write(Integer.toString(answer));
+					bw.flush();
+					return;
+				}
+				for(int i=0;i<delta.length;i++) {
+					int nr = point[0]+delta[i][0], nc = point[1]+delta[i][1];
+					if(isIn(nr,nc)&&!visit[nr][nc][point[2]]&&map[nr][nc]==0) {
+						visit[nr][nc][point[2]]=true;
+						dq.offerLast(new int[]{nr, nc, point[2]});
+					}
+				}
+				if(point[2]<K) {
+					for(int i=0;i<knight.length;i++) {
+						int nr = point[0]+knight[i][0], nc = point[1]+knight[i][1];
+						if(isIn(nr,nc)&&!visit[nr][nc][point[2]+1]&&map[nr][nc]==0) {
+							visit[nr][nc][point[2]+1]=true;
+							dq.offerLast(new int[] {nr,nc,point[2]+1});
+						}
+					}
+				}
+			}
+			answer++;
+		}
+		bw.write("-1");
+		bw.flush();
+	}
+	static boolean isIn(int r, int c) {
+		return 0<=r&&r<N&&0<=c&&c<M;
+	}
 }
