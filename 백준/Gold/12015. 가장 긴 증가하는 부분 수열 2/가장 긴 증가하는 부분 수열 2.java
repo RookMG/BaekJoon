71cc172395acaa92f10fd1f3e1e84c29f16a4ad6
RookMG <tngks1995@naver.com>
2023-03-31 11:03:23 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 672 ms, Memory: 151524 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,37 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main{
-    public void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(0);
-        for(int i=0;i<n;i++){
-            int num = Integer.parseInt(st.nextToken());
-            if(num>list.get(list.size()-1)){
-                list.add(num);
-            }
-            else{
-                int left = 0, right = list.size()-1;
-                while(1+left<right){
-                    int mid = (left+right)/2;
-                    if(list.get(mid)>=num){
-                        right = mid;
-                    }else{
-                        left = mid;
-                    }
-                }
-                list.set(right,num);
-            }
-        }
-        System.out.println(list.size()-1);
-    }
-    public static void main(String[] args) throws Exception{
-        new Main().solution();
-    }
+import java.io.*;
+import java.util.*;
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(-1000000001);
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++){
+			int num = arr[i] = Integer.parseInt(st.nextToken());
+			if(num>list.get(list.size()-1)){
+				list.add(num);
+				continue;
+			}
+			int left = 0, right = list.size()-1;
+			while(1+left<right){
+				int mid = (left+right)/2;
+				if(list.get(mid)>=num){
+					right = mid;
+				}else{
+					left = mid;
+				}
+			}
+			list.set(right,num);
+		}
+		bw.write(Integer.toString(list.size()-1));
+		bw.flush();
+	}
 }
