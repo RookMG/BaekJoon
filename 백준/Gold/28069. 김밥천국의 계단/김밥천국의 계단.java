94142287c41f4e62ec60ce6ca9692aae66ca6be4
RookMG <tngks1995@naver.com>
2023-06-01 22:41:21 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -7,16 +7,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
-        int[] dp = new int[N+1];
-        Arrays.fill(dp,1000001);
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=1;i<N;i++){
-            dp[i+1] = Math.min(dp[i+1],dp[i]+1);
-            if(i+(i>>1)>N) continue;
-            dp[i+(i>>1)] = Math.min(dp[i+(i>>1)],dp[i]+1);
-        }
-        bw.write(dp[N]>K?"water":"minigimbob");
+        for(;K<N&&K>0;K--) N = N%3==2?N-1:(N/3*2+N%3);
+        bw.write(N>K?"water":"minigimbob");
         bw.flush();
     }
 }
