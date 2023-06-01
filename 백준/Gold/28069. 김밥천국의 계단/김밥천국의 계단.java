7fbd1229534980076454c1a7537641cbdce0be76
RookMG <tngks1995@naver.com>
2023-06-01 22:21:07 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 132 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -7,28 +7,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
-        boolean[] visit = new boolean[N+1];
-        String ans = "water";
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.offerFirst(0);
-        bfs:for(int time = 0, now;!q.isEmpty()&&time<=K;time++){
-            for(int size = q.size();size>0;size--){
-                now = q.pollFirst();
-                if(now==N){
-                    ans = "minigimbob";
-                    break bfs;
-                }
-                if(now<N&&!visit[now+1]){
-                    visit[now+1]=true;
-                    q.offerLast(now+1);
-                }
-                if(now+(now>>1)<=N&&!visit[now+(now>>1)]){
-                    visit[now+(now>>1)]=true;
-                    q.offerLast(now+(now>>1));
-                }
-            }
+        int[] dp = new int[N+1];
+        Arrays.fill(dp,1000001);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=1;i<N;i++){
+            dp[i+1] = Math.min(dp[i+1],dp[i]+1);
+            if(i+(i>>1)>N) continue;
+            dp[i+(i>>1)] = Math.min(dp[i+(i>>1)],dp[i]+1);
         }
-        bw.write(ans);
+        bw.write(dp[N]>K?"water":"minigimbob");
         bw.flush();
     }
 }
