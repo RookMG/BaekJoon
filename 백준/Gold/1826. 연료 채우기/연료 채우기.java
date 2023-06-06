98750d4f55f041e95b6b772eb651948cfd61655c
RookMG <tngks1995@naver.com>
2023-06-06 23:57:28 +0900

    [Gold II] Title: 연료 채우기, Time: 188 ms, Memory: 17180 KB -BaekjoonHub

--- 백준/Gold/1826. 연료 채우기/연료 채우기.java
+++ 백준/Gold/1826. 연료 채우기/연료 채우기.java
@@ -6,21 +6,23 @@ public class Main {
     static StringTokenizer st;
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine()), ans = 0;
-        PriorityQueue<int[]> stops = new PriorityQueue<>(new Comparator<int[]>() {
+        int[][] stops = new int[N][2];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            stops[i][0] = Integer.parseInt(st.nextToken());
+            stops[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(stops, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o1[0]-o2[0];
             }
         });
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            stops.add(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-        }
         st = new StringTokenizer(br.readLine());
         int goal = Integer.parseInt(st.nextToken()), fuel = Integer.parseInt(st.nextToken());
-        for(;fuel<goal;ans++,fuel-=pq.poll()){
-            while(!stops.isEmpty()&&stops.peek()[0]<=fuel) pq.add(-stops.poll()[1]);
+        for(int i=0;fuel<goal;ans++,fuel-=pq.poll()){
+            while(i<N&&stops[i][0]<=fuel) pq.add(-stops[i++][1]);
             if(pq.isEmpty()){
                 ans = -1;
                 break;
