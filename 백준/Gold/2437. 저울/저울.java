3c64d2b981d2eafe3929598ec815cdf8b57c3f75
RookMG <tngks1995@naver.com>
2023-06-09 15:09:29 +0900

    [Gold II] Title: 저울, Time: 84 ms, Memory: 11900 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine()), ans = 1;
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        for(int num:arr){
+            if(num>ans) break;
+            ans+= num;
+        }
+        bw.write(Integer.toString(ans));
+        bw.flush();
+    }
+}
