16d95ccf4e0e8346dec95bdc72df78ea36134b29
RookMG <tngks1995@naver.com>
2023-06-09 16:55:04 +0900

    [Gold I] Title: 동전 뒤집기, Time: 136 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -8,12 +8,11 @@ public class Main {
         for(int i=0;i<N;i++){
             for(char ch: br.readLine().toCharArray()) map[i] = (map[i]<<1) + (ch=='T'?1:0);
         }
-        for(int i=(1<<N)-1,now=0,cnt;i>=0;i--,now=0){
+        for(int i=(1<<N)-1,now=0,cnt;i>=0;i--,ans = Math.min(ans,now),now=0){
             for(int line:map) {
                 cnt = Integer.bitCount(i^line);
                 now += Math.min(cnt,N-cnt);
             }
-            ans = Math.min(ans,now);
         }
         bw.write(Integer.toString(ans));
         bw.flush();
