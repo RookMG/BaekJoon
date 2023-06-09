ebfe32abe616371f097b6151aa1b9a9c46df869b
RookMG <tngks1995@naver.com>
2023-06-09 16:54:29 +0900

    [Gold I] Title: 동전 뒤집기, Time: 140 ms, Memory: 12260 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -1,21 +1,14 @@
 import java.io.*;
-import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int[] map;
-    static int N, ans=400;
     public static void main(String[] args) throws Exception {
-        N = Integer.parseInt(br.readLine());
-        map = new int[N];
+        int N = Integer.parseInt(br.readLine()), ans = 200;
+        int[] map = new int[N];
         for(int i=0;i<N;i++){
-            for(char ch: br.readLine().toCharArray()){
-                map[i]<<=1;
-                map[i] += ch=='T'?1:0;
-            }
+            for(char ch: br.readLine().toCharArray()) map[i] = (map[i]<<1) + (ch=='T'?1:0);
         }
-        for(int i=(1<<N)-1, now, cnt;i>=0;i--){
-            now = 0;
+        for(int i=(1<<N)-1,now=0,cnt;i>=0;i--,now=0){
             for(int line:map) {
                 cnt = Integer.bitCount(i^line);
                 now += Math.min(cnt,N-cnt);
