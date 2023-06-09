f83b30e8f7cda3dfd5c8cde53ac12cb70613980a
RookMG <tngks1995@naver.com>
2023-06-09 16:16:03 +0900

    [Gold III] Title: 순회강연, Time: 196 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static class Node{
+        int deadline, value;
+        Node(int value, int deadline) {
+            this.deadline = deadline;
+            this.value = value;
+        }
+    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Node> deadline = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return Integer.compare(o1.deadline,o2.deadline);
+            }
+        }), selected = new PriorityQueue<>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return Integer.compare(o1.value,o2.value);
+            }
+        });
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            deadline.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        for(Node node;!deadline.isEmpty();){
+            node = deadline.poll();
+            if(selected.size()<node.deadline) selected.add(node);
+            else if(selected.peek().value<node.value){
+                selected.poll();
+                selected.add(node);
+            }
+        }
+        long ans = 0L;
+        for(Node node:selected) ans += node.value;
+        bw.write(Long.toString(ans));
+        bw.flush();
+    }
+}
