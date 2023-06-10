9af275cdfcdb05c7d274cf13d3038dcbd3d31a33
RookMG <tngks1995@naver.com>
2023-06-10 15:21:46 +0900

    [Platinum III] Title: 문자열 장식, Time: 176 ms, Memory: 32936 KB -BaekjoonHub

--- 백준/Platinum/1294. 문자열 장식/문자열 장식.java
+++ 백준/Platinum/1294. 문자열 장식/문자열 장식.java
@@ -3,19 +3,18 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
-    static StringBuffer bf = new StringBuffer();
+    static StringBuilder sb = new StringBuilder(), tmp = new StringBuilder();
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine());
         PriorityQueue<String> pq = new PriorityQueue<String>();
         for(int i=0;i<N;i++) pq.add(br.readLine()+"[");
         while(!pq.isEmpty()){
-            bf.setLength(0);
-            bf.append(pq.poll());
-            sb.append(bf.charAt(0));
-            if(bf.length()==2) continue;
-            bf.deleteCharAt(0);
-            pq.add(bf.toString());
+            tmp.setLength(0);
+            tmp.append(pq.poll());
+            sb.append(tmp.charAt(0));
+            if(tmp.length()==2) continue;
+            tmp.deleteCharAt(0);
+            pq.add(tmp.toString());
         }
         bw.write(sb.toString());
         bw.flush();
