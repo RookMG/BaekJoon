ee0604f54d3e71b038961e41f434402192c5edda
RookMG <tngks1995@naver.com>
2023-06-11 11:53:33 +0900

    [Gold II] Title: 로고, Time: 120 ms, Memory: 12492 KB -BaekjoonHub

--- 백준/Gold/3108. 로고/로고.java
+++ 백준/Gold/3108. 로고/로고.java
@@ -11,7 +11,7 @@ public class Main {
         }
         boolean isCrossing(Rect r){
             return (x2>=r.x1&&r.x2>=x1&&y2>=r.y1&&r.y2>=y1)
-                    &&!(((x1<r.x1&&r.x2<x2)&&(y1<r.y1&&r.y2<y2))||((r.x1<x1&&x2<r.x2)&&(r.y1<y1&&y2<r.y2)));
+                    &&(x1>=r.x1||r.x2>=x2||y1>=r.y1||r.y2>=y2)&&(r.x1>=x1||x2>=r.x2||r.y1>=y1||y2>=r.y2);
         }
     }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
