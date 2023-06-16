4eeb3dff483a35260a94fef0fd8ffc19b1f24d94
RookMG <tngks1995@naver.com>
2023-06-16 17:03:34 +0900

    [Gold V] Title: 문자열 판별, Time: 80 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Gold/16500. 문자열 판별/문자열 판별.java
+++ 백준/Gold/16500. 문자열 판별/문자열 판별.java
@@ -3,45 +3,39 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static String line, key;
-    static String[] input;
+    static char[] line, key;
     static int[] pi, dp;
-    static boolean[][] fit;
+    static ArrayList<ArrayList<Integer>> pos;
     public static void main(String[] args) throws Exception{
-        line = br.readLine();
-        dp = new int[line.length()+1];
-        pi = new int[10000];
-        int N = Integer.parseInt(br.readLine());
-        input = new String[N];
-        fit = new boolean[N][line.length()];
-        for(int i=0;i<N;i++){
-            key = input[i] = br.readLine();
+        line = br.readLine().toCharArray();
+        pos = new ArrayList<>();
+        dp = new int[line.length+1];
+        for(int i=0;i<line.length;i++) pos.add(new ArrayList<>());
+        for(int i= Integer.parseInt(br.readLine());i>0;i--){
+            key = br.readLine().toCharArray();
+            pi = new int[key.length];
             getPi();
-            kmp(i);
+            kmp();
         }
-        for(int i=0;i<line.length();i++) {
+        for(int i=0;i<line.length;i++) {
             dp[i+1] = Math.max(dp[i+1],dp[i]);
-            for(int j=0;j<N;j++) {
-                if(!fit[j][i]) continue;
-                dp[i+input[j].length()] = Math.max(dp[i+input[j].length()],dp[i]+input[j].length());
-            }
+            for(int l:pos.get(i)) dp[i+l] = Math.max(dp[i+l],dp[i]+l);
         }
-        bw.write(line.length()==dp[line.length()]?'1':'0');
+        bw.write(line.length==dp[line.length]?'1':'0');
         bw.flush();
     }
     static void getPi(){
-        Arrays.fill(pi,0);
-        for(int i=1,j=0;i<key.length();i++){
-            while(j>0&&key.charAt(i)!=key.charAt(j)) j = pi[j-1];
-            if(key.charAt(i)==key.charAt(j)) pi[i] = ++j;
+        for(int i=1,j=0;i<pi.length;i++){
+            while(j>0&&key[i]!=key[j]) j = pi[j-1];
+            if(key[i]==key[j]) pi[i] = ++j;
         }
     }
-    static void kmp(int idx){
-        for(int i=0,j=0;i<line.length();i++){
-            while(j>0&&line.charAt(i)!=key.charAt(j)) j=pi[j-1];
-            if(line.charAt(i)!=key.charAt(j)) continue;
-            if(j==key.length()-1){
-                fit[idx][i-j] = true;
+    static void kmp(){
+        for(int i=0,j=0;i<line.length;i++){
+            while(j>0&&line[i]!=key[j]) j=pi[j-1];
+            if(line[i]!=key[j]) continue;
+            if(j==key.length-1){
+                pos.get(i-j).add(key.length);
                 j = pi[j];
                 continue;
             }
