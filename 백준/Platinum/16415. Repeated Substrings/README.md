ebc0d042bcb03d853a49f0c32f34427cddfca886
RookMG <tngks1995@naver.com>
2023-06-22 13:14:27 +0900

    [Platinum III] Title: Repeated Substrings, Time: 396 ms, Memory: 23712 KB -BaekjoonHub

---
+++ 백준/Platinum/16415. Repeated Substrings/README.md
@@ -0,0 +1,24 @@
+# [Platinum III] Repeated Substrings - 16415 
+
+[문제 링크](https://www.acmicpc.net/problem/16415) 
+
+### 성능 요약
+
+메모리: 23712 KB, 시간: 396 ms
+
+### 분류
+
+문자열, 접미사 배열과 LCP 배열
+
+### 문제 설명
+
+<p>Given an input string composed solely of lowercase English letters, find the longest substring that occurs more than once in the input string. The two occurrences are allowed to partially overlap.</p>
+
+### 입력 
+
+ <p>The input is a single line containing a string of lowercase letters. The string contains more than one character, but no more than 10<sup>5</sup>. At least one letter will appear at least twice.</p>
+
+### 출력 
+
+ <p>Print a single line of output: the longest substring that occurs more than once in the input string. If there are multiple longest repeated substrings, print the one the would come first when the longest substrings are sorted in lexicographical (alphabetical) order.</p>
+
---
+++ 백준/Platinum/16415. Repeated Substrings/Repeated Substrings.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int[] r, nr, cnt, sa, idx, lcp, isa;
+    static char[] line;
+    public static void main(String[] args) throws Exception {
+        line = br.readLine().toCharArray();
+        N = line.length;
+        getSA();
+        getlcp();
+        ArrayList<String> ans = new ArrayList<>();
+        int[] cpy = Arrays.copyOf(lcp,N);
+        Arrays.sort(cpy);
+        for(int i=0;i<N;i++){
+            if(lcp[i]!=cpy[N-1]) continue;
+            sb.setLength(0);
+            for(int j=0;j<lcp[i];j++) sb.append(line[sa[i]+j]);
+            ans.add(sb.toString());
+        }
+        Collections.sort(ans);
+        bw.write(ans.get(0));
+        bw.flush();
+    }
+    static void getSA(){
+        int M = Math.max(257,N+1);
+        sa = new int[N];
+        r = new int[2*N];
+        nr = new int[2*N];
+        cnt = new int[M];
+        idx = new int[N];
+        for(int i=0;i<N;++i){
+            sa[i] = i;
+            r[i] = line[i];
+        }
+        for(int d=1;d<N;d<<=1){
+            Arrays.fill(cnt,0);
+            for(int i=0;i<N;++i) cnt[r[i+d]]++;
+            for(int i=1;i<M;++i) cnt[i] += cnt[i-1];
+            for(int i=N-1;i>=0;--i) idx[--cnt[r[i+d]]] = i;
+            Arrays.fill(cnt,0);
+            for(int i=0;i<N;++i) cnt[r[i]]++;
+            for(int i=1;i<M;++i) cnt[i]+=cnt[i-1];
+            for(int i=N-1;i>=0;--i) sa[--cnt[r[idx[i]]]] = idx[i];
+            nr[sa[0]] = 1;
+            for(int i=1;i<N;++i) nr[sa[i]] = nr[sa[i-1]] + ((r[sa[i-1]]<r[sa[i]]||(r[sa[i-1]]==r[sa[i]]&&r[sa[i-1]+d]<r[sa[i]+d]))?1:0);
+            for(int i=0;i<N;++i) r[i] = nr[i];
+            if(r[sa[N-1]]==N) break;
+        }
+    }
+    static void getlcp(){
+        lcp = new int[N];
+        isa = new int[N];
+        for(int i=0;i<N;++i) isa[sa[i]] = i;
+        for(int k=0, i=0;i<N;++i){
+            if(isa[i]>0){
+                for(int j=sa[isa[i]-1];Math.max(i+k,j+k)<N&&line[i+k]==line[j+k];++k);
+                lcp[isa[i]] = (k!=0 ? k-- : 0);
+            }
+        }
+    }
+}
