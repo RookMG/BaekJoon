aa2ebc70c2fb13d0bf6f3d481e18cea36a3661c6
RookMG <tngks1995@naver.com>
2023-06-29 21:27:01 +0900

    [Platinum IV] Title: 트리와 경로의 길이, Time: 88 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Platinum/12928. 트리와 경로의 길이/트리와 경로의 길이.java
+++ 백준/Platinum/12928. 트리와 경로의 길이/트리와 경로의 길이.java
@@ -5,26 +5,25 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int N, S;
-    static int[][] dp;
+    static boolean[][] visit;
     static char ans = '0';
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
-        dp = new int[N+1][S+1];
+        visit = new boolean[N+1][S+1];
         recur(N-2,0);
         bw.write(ans);
         bw.flush();
     }
 
     static void recur(int n, int cnt){
-        if(ans=='1') return;
         if(n==0&&cnt==S){
             ans = '1';
             return;
         }
-        if(n==0||cnt>S||dp[n][cnt]>0) return;
-        dp[n][cnt] = 1;
+        if(ans=='1'||n==0||cnt>S||visit[n][cnt]) return;
+        visit[n][cnt] = true;
         for(int i=1;i<=n;i++) recur(n-i,cnt + (i*(i+1)>>1));
     }
 }
