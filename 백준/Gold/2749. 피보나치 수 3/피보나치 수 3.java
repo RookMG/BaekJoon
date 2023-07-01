91e32b1bb385e353ff03bf231c99821ac2f212b3
RookMG <tngks1995@naver.com>
2023-07-01 17:08:54 +0900

    [Gold II] Title: 피보나치 수 3, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final long MOD = 1000000;
+    static long[][] mat = {{1,1},{1,0}};
+    public static void main(String[] args) throws Exception {
+        long N = Long.parseLong(br.readLine());
+        bw.write(Long.toString(N==1?1:matPow(mat,N-1)[0][0]));
+        bw.flush();
+    }
+    static long[][] matMul(long[][] a, long[][] b){
+        long[][] c = new long[b[0].length][a.length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                for(int k=0;k<b[0].length;k++){
+                    c[i][k] = (c[i][k]+(a[i][j]*b[j][k])%MOD)%MOD;
+                }
+            }
+        }
+        return c;
+    }
+    static long[][] matPow(long[][] a, long num){
+        if(num==1) return a;
+        else if(num%2==1) return matMul(matPow(a,num-1),a);
+        else return matPow(matMul(a,a),num/2);
+    }
+}
