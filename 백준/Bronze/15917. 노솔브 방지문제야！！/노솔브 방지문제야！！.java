0eb2c76d2984498a2adc8c5e5d2331e6539b58ce
RookMG <tngks1995@naver.com>
2023-07-02 11:53:31 +0900

    [Bronze III] Title: 노솔브 방지문제야!!, Time: 396 ms, Memory: 81144 KB -BaekjoonHub

--- 백준/Bronze/15917. 노솔브 방지문제야！！/노솔브 방지문제야！！.java
+++ 백준/Bronze/15917. 노솔브 방지문제야！！/노솔브 방지문제야！！.java
@@ -4,7 +4,10 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
-        for(int T = Integer.parseInt(br.readLine());T>0;T--) sb.append(Integer.bitCount(Integer.parseInt(br.readLine()))==1?"1\n":"0\n");
+        for(int T = Integer.parseInt(br.readLine()),now;T>0;T--){
+            now = Integer.parseInt(br.readLine());
+            sb.append((now&(-now))==now?"1\n":"0\n");
+        }
         bw.write(sb.toString());
         bw.flush();
     }
