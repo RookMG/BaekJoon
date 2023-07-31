731acf541d5c891f7eabb3d24e7590af5c39d281
RookMG <tngks1995@naver.com>
2023-07-31 17:43:58 +0900

    [Bronze IV] Title: 히스토그램, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/13752. 히스토그램/히스토그램.java
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -1,11 +1,9 @@
 import java.io.*;
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
-        for(int i= Integer.parseInt(br.readLine());i>0;i--, sb.append('\n')) for(int j= Integer.parseInt(br.readLine());j>0;j--) sb.append("=");
-        bw.write(sb.toString());
-        bw.flush();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for(int i=Integer.parseInt(br.readLine());i>0;i--,sb.append('\n')) for(int j=Integer.parseInt(br.readLine());j>0;j--) sb.append("=");
+        System.out.print(sb);
     }
 }
