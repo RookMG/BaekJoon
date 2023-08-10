f663bc929e086532bd2251ca7973a254a82b2d46
RookMG <tngks1995@naver.com>
2023-08-10 09:36:47 +0900

    [Silver V] Title: 색종이, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,25 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main{
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static boolean[][] board = new boolean[101][101];
+import java.io.*;
+import java.util.*;
+public class Main {
     public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        boolean[][] board = new boolean[101][101];
         int n = Integer.parseInt(br.readLine()), answer = 0;
-        for(int i=0;i<n;i++) {
-	        st = new StringTokenizer(br.readLine());
-	        int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
-	        for(int r=0;r<10;r++) {
-	        	for(int c=0;c<10;c++) {
-	        		if(!board[x+r][y+c]) {
-		        		board[x+r][y+c] = true;
-		        		answer++;
-	        		}
-	        	}
-	        }
+        for(int i=0, x, y;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            for(int r=x+9;r>=x;r--) {
+                for(int c=y+9;c>=y;board[r][c] = true,c--){
+                    if(!board[r][c]) answer++;
+                }
+            }
         }
-        System.out.print(answer);
+        bw.write(Integer.toString(answer));
+        bw.flush();
     }
 }
