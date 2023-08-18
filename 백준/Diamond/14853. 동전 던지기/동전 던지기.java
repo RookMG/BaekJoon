59bda539352751419d09b8cd993da77cee92e48a
RookMG <tngks1995@naver.com>
2023-08-18 16:38:29 +0900

    [Diamond II] Title: 동전 던지기, Time: 1240 ms, Memory: 89748 KB -BaekjoonHub

--- 백준/Diamond/14853. 동전 던지기/동전 던지기.java
+++ 백준/Diamond/14853. 동전 던지기/동전 던지기.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         for(int T= Integer.parseInt(br.readLine());T>0;T--){
             st = new StringTokenizer(br.readLine());
-            int n1 = Integer.parseInt(st.nextToken()), m1 = Integer.parseInt(st.nextToken()), n2 = Integer.parseInt(st.nextToken()), m2 = Integer.parseInt(st.nextToken());
+            int n1 = Integer.parseInt(st.nextToken())+1, m1 = Integer.parseInt(st.nextToken())+1, n2 = Integer.parseInt(st.nextToken())+1, m2 = Integer.parseInt(st.nextToken());
             double ans = 1.0, mul;
-            for(int i=0;i<=m1;i++) ans*=1.0*(n1+1-i)/(n1+n2+2-i);
+            for(int i=0;i<m1;i++) ans=ans*(n1-i)/(n1+n2-i);
             mul = ans;
-            for(int i=0;i<m2;i++) ans+=mul*=1.0*(m1+i+1)/(i+1)*(n2+1-i)/(n1+n2-m1+1-i);
+            for(int i=0;i<m2;i++) ans+=mul=mul*(m1+i)/(i+1)*(n2-i)/(n1+n2-m1-i);
             sb.append(ans).append('\n');
         }
         bw.write(sb.toString());
