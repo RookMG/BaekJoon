5e202d1e920935a6b2b55407c7acd6d08546e1f5
RookMG <tngks1995@naver.com>
2023-08-24 08:39:39 +0900

    [Gold IV] Title: 전력난, Time: 952 ms, Memory: 250420 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.java
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -7,29 +7,30 @@ public class Main {
     static StringTokenizer st;
     static int[] parent;
     public static void main(String[] args) throws Exception {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[2], o2[2]);
+            }
+        });
         while(true){
             st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken()), ans = 0;
             if(M==0&&N==0) break;
             parent = new int[M];
             for (int i = 0; i < M; i++) parent[i] = i;
-            PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return Integer.compare(o1[2], o2[2]);
-                }
-            });
+            pq.clear();
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 int[] arr = {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
                 ans += arr[2];
                 pq.offer(arr);
             }
-            for (int i = 1; i < M; ) {
+            for (int i = 1, ap, bp; i < M; ) {
                 int[] now = pq.poll();
-                if (findP(now[0]) == findP(now[1])) continue;
+                if ((ap=findP(now[0])) == (bp=findP(now[1]))) continue;
                 i++;
-                parent[findP(now[0])] = parent[findP(now[1])];
+                parent[Math.min(ap,bp)] = parent[Math.max(ap,bp)];
                 ans -= now[2];
             }
             sb.append(ans).append('\n');
