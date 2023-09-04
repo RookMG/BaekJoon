83cee8d2d310566d157ad6b9568921357aeb52eb
RookMG <tngks1995@naver.com>
2023-09-04 12:56:32 +0900

    [Gold III] Title: 과제, Time: 152 ms, Memory: 14712 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -10,7 +10,6 @@ public class Main {
     }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine());
