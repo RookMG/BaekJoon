9e56a8dbc98eb5472573be9cb1200951a42b9acf
RookMG <tngks1995@naver.com>
2023-11-01 09:36:16 +0900

    [Gold IV] Title: 가장 먼 곳, Time: 1216 ms, Memory: 203164 KB -BaekjoonHub

--- 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.java
+++ 백준/Gold/22865. 가장 먼 곳/가장 먼 곳.java
@@ -1,26 +1,35 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static class Node implements Comparable<Node>{
+        int idx;
+        long dist;
+
+        public Node(int idx, long dist) {
+            this.idx = idx;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Long.compare(dist,o.dist);
+        }
+    }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     public static void main(String[] args) throws Exception {
-        PriorityQueue<long[]> pq = new PriorityQueue<>(new Comparator<long[]>() {
-            @Override
-            public int compare(long[] o1, long[] o2) {
-                return Long.compare(o1[1],o2[1]);
-            }
-        });
+        PriorityQueue<Node> pq = new PriorityQueue<>();
         ArrayList<ArrayList<int[]>> links = new ArrayList<>();
         int N = Integer.parseInt(br.readLine());
-        long[] cost = new long[N+1];
-        Arrays.fill(cost,Integer.MAX_VALUE/2);
+        long[] dist = new long[N+1];
+        Arrays.fill(dist,Integer.MAX_VALUE/2);
         for(int i=0;i<=N;i++) links.add(new ArrayList<>());
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<3;i++){
             int start = Integer.parseInt(st.nextToken());
-            pq.offer(new long[]{start,0});
-            cost[start] = 0;
+            pq.offer(new Node(start, 0));
+            dist[start] = 0;
         }
         for(int i = Integer.parseInt(br.readLine());i>0;i--){
             st = new StringTokenizer(br.readLine());
@@ -29,19 +38,18 @@ public class Main {
             links.get(b).add(new int[] {a,d});
         }
         while(!pq.isEmpty()){
-            long[] now = pq.poll();
-            if(cost[(int)now[0]]<now[1]) continue;
-            for(int[] next: links.get((int)now[0])){
-                if(cost[next[0]]<=now[1]+next[1]) continue;
-                cost[next[0]] = now[1]+next[1];
-                pq.offer(new long[]{next[0],cost[next[0]]});
+            Node now = pq.poll();
+            if(dist[now.idx]<now.dist) continue;
+            for(int[] next: links.get(now.idx)){
+                if(dist[next[0]]<=now.dist+next[1]) continue;
+                pq.offer(new Node(next[0],dist[next[0]] = now.dist+next[1]));
             }
         }
         int ans = 0;
-        long dist = 0;
+        long longest = 0;
         for(int i=1;i<=N;i++){
-            if(cost[i]<=dist) continue;
-            dist = cost[i];
+            if(dist[i]<=longest) continue;
+            longest = dist[i];
             ans = i;
         }
         bw.write(Integer.toString(ans));
