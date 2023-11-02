81b4c9648a043b13c69c81eeec0911f6ab2c0b37
RookMG <tngks1995@naver.com>
2023-11-02 09:00:10 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -4,15 +4,13 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine()), ans = 0;
-        int[] arr = new int[N];
-        for(int i=0;i<N;i++) arr[i] = Integer.parseInt(br.readLine());
-        int max = arr[N-1], diff;
-        for(int i=N-2;i>=0;i--){
-            diff = arr[i] - max +1;
-            if(diff>0){
-                max--;
-                ans+=diff;
-            }else max = arr[i];
+        int[] arr = new int[N-1];
+        for(int i=N-2;i>=0;i--) arr[i] = Integer.parseInt(br.readLine());
+        int max = Integer.parseInt(br.readLine()), diff;
+        for(int next:arr){
+            diff = next - max +1;
+            ans += Math.max(0,diff);
+            max = diff>0?max-1:next;
         }
         bw.write(Integer.toString(ans));
         bw.flush();
