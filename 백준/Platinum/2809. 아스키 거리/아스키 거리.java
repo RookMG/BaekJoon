07a57d44809edfbec97967427dd731aaeff7e509
RookMG <tngks1995@naver.com>
2023-11-08 13:25:33 +0900

    [Platinum I] Title: 아스키 거리, Time: 2472 ms, Memory: 861440 KB -BaekjoonHub

--- 백준/Platinum/2809. 아스키 거리/아스키 거리.java
+++ 백준/Platinum/2809. 아스키 거리/아스키 거리.java
@@ -69,7 +69,7 @@ public class Main {
             }
         }
     }
-    static final int SIZE = 1250;
+    static final int SIZE = 1667;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static boolean[] found;
