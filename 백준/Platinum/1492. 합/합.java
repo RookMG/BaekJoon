bc193aec0d6d417a4dd5b3ee2aa08b77daa2364d
RookMG <tngks1995@naver.com>
2023-11-20 10:50:29 +0900

    [Platinum II] Title: 합, Time: 80 ms, Memory: 11672 KB -BaekjoonHub

---
+++ 백준/Platinum/1492. 합/합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static long MOD = 1_000_000_007;
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        long[] dp = new long[K+1], com = new long[K+2];
+        Arrays.fill(com,1);
+        dp[0] = N;
+        for(int i=1;i<=K;i++){
+            long psum = N;
+            com[i] = (com[i]+com[i-1])%MOD;
+            for(int j=i-1;j>0;j--){
+                com[j] = (com[j]+com[j-1])%MOD;
+                psum = (psum + com[j]*dp[j]%MOD)%MOD;
+            }
+            dp[i] = pow(com[i],MOD-2)*(MOD+pow(N+1,i+1)-1-psum)%MOD;
+        }
+        bw.write(Long.toString(dp[K]));
+        bw.flush();
+    }
+
+    static long pow(long base, long exp){
+        if(exp==0) return 1;
+        else if(exp == 1) return base;
+        long ret = pow(base,exp>>1);
+        ret = ret*ret%MOD;
+        if((exp&1) == 1) ret = ret*base%MOD;
+        return ret;
+    }
+}
