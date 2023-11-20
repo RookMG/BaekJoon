5eca61a58a5bf24524398a908e8f9bbddabab9b4
RookMG <tngks1995@naver.com>
2023-11-20 09:40:03 +0900

    [Gold II] Title: 확장 게임, Time: 452 ms, Memory: 52660 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -1,13 +1,6 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static class Node{
-        int r, c;
-        Node(int r, int c){
-            this.r = r;
-            this.c = c;
-        }
-    }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringBuilder sb = new StringBuilder();
@@ -20,7 +13,7 @@ public class Main {
         C = Integer.parseInt(st.nextToken());
         P = Integer.parseInt(st.nextToken());
         int[] limits = new int[P], cnt = new int[P];
-        ArrayDeque<Node>[] dqs = new ArrayDeque[P];
+        ArrayDeque<int[]>[] dqs = new ArrayDeque[P];
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<P;i++){
             limits[i] = Integer.parseInt(st.nextToken());
@@ -31,34 +24,32 @@ public class Main {
             map[r] = br.readLine().toCharArray();
             for(int c=0;c<C;c++){
                 if(map[r][c]<'1'||map[r][c]>'9') continue;
-                dqs[map[r][c]-'1'].offerLast(new Node(r, c));
+                dqs[map[r][c]-'1'].offerLast(new int[]{r, c});
                 cnt[map[r][c]-'1']++;
             }
         }
-        boolean progress;
-        do{
-            progress = false;
+        for(boolean progress = true;progress;){
+            progress=false;
             for(int p=1;p<=P;p++){
                 for(int l=0;l<limits[p-1]&&dqs[p-1].size()>0;l++){
                     for(int s=dqs[p-1].size();s>0;s--){
-                        Node now = dqs[p-1].pollFirst();
+                        int[] now = dqs[p-1].pollFirst();
                         for(int d=0;d<delta.length;d++){
-                            int nr = now.r+delta[d][0], nc = now.c+delta[d][1];
+                            int nr = now[0]+delta[d][0], nc = now[1]+delta[d][1];
                             if(!isIn(nr,nc)||map[nr][nc]!='.') continue;
                             progress = true;
-                            dqs[p-1].offerLast(new Node(nr,nc));
+                            dqs[p-1].offerLast(new int[]{nr,nc});
                             map[nr][nc] = (char)('0'+p);
                             cnt[p-1]++;
                         }
                     }
                 }
             }
-        }while(progress);
+        }
         for(int i:cnt) sb.append(i).append(' ');
         bw.write(sb.toString());
         bw.flush();
     }
-
     static boolean isIn(int r, int c) {
         return 0 <= r && r < R && 0 <= c && c < C;
     }
