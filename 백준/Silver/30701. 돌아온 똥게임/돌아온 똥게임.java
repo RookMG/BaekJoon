9b5734590e6dc141cd617aeb44ad5a8357eaf6a9
RookMG <tngks1995@naver.com>
2023-12-07 15:17:28 +0900

    [Silver III] Title: 돌아온 똥게임, Time: 436 ms, Memory: 40704 KB -BaekjoonHub

---
+++ 백준/Silver/30701. 돌아온 똥게임/돌아온 똥게임.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), ans = 0;
+        long D = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> enemy = new PriorityQueue<>(), weapon = new PriorityQueue<>();
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            if(Integer.parseInt(st.nextToken())==1) enemy.offer(Integer.parseInt(st.nextToken()));
+            else weapon.offer(Integer.parseInt(st.nextToken()));
+        }
+        while(!enemy.isEmpty()){
+            if(enemy.peek()>=D){
+                if(weapon.isEmpty()) break;
+                D *= weapon.poll();
+                ans++;
+                continue;
+            }
+            D += enemy.poll();
+            ans++;
+        }
+        ans += weapon.size();
+        bw.write(Integer.toString(ans));
+        bw.flush();
+    }
+}
