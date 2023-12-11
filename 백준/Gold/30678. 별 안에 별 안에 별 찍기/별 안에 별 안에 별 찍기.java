ea2d76e4bdc1aac18078360975e8a3d874bf2ea2
RookMG <tngks1995@naver.com>
2023-12-11 18:14:53 +0900

    [Gold V] Title: 별 안에 별 안에 별 찍기, Time: 272 ms, Memory: 125920 KB -BaekjoonHub

--- 백준/Gold/30678. 별 안에 별 안에 별 찍기/별 안에 별 안에 별 찍기.java
+++ 백준/Gold/30678. 별 안에 별 안에 별 찍기/별 안에 별 안에 별 찍기.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringBuilder sb = new StringBuilder(), blank = new StringBuilder();
+    static StringBuilder sb = new StringBuilder();
     static final boolean[][] star = {{false,false,true,false,false},{false,false,true,false,false},{true,true,true,true,true},{false,true,true,true,false},{false,true,false,true,false}};
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine());
@@ -11,12 +11,13 @@ public class Main {
         now.add("*");
         for(int i=1, size = 1;i<=N;i++, size*=5){
             next.clear();
-            blank.setLength(0);
-            for(int j=0;j<size;j++) blank.append(' ');
+            sb.setLength(0);
+            for(int j=0;j<size;j++) sb.append(' ');
+            String blank = sb.toString();
             for(int r=0;r<5;r++){
                 for(int j=0;j<size;j++) {
                     sb.setLength(0);
-                    for (int c = 0; c < 5; c++) sb.append(star[r][c]?now.get(j):blank.toString());
+                    for (int c = 0; c < 5; c++) sb.append(star[r][c]?now.get(j):blank);
                     next.add(sb.toString());
                 }
             }
