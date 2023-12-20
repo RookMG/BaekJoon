77ea14eb57b0303b9dcf04ab6bdb0682200a07ff
RookMG <tngks1995@naver.com>
2023-12-20 10:40:29 +0900

    [Platinum IV] Title: 동전 탑 게임, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Platinum/29617. 동전 탑 게임/동전 탑 게임.java
+++ 백준/Platinum/29617. 동전 탑 게임/동전 탑 게임.java
@@ -7,16 +7,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
-        boolean first;
-        if(k>=0){
-            first = a!=b;
-        }else{
-            if(a+b==1) first = false;
-            else if(a==b&&a==1) first = true;
-            else if(k==-1) first = true;
-            else first = a!=b;
-        }
-        bw.write(first?"First":"Second");
+        bw.write((k<0?(a+b!=1&&((a==b&&a==1)||k==-1||a!=b)):a!=b)?"First":"Second");
         bw.flush();
     }
 }
