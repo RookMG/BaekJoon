106bae517c75105db06025a17a9448c456aec792
RookMG <tngks1995@naver.com>
2024-01-02 11:31:18 +0900

    [Silver I] Title: ESC, Time: 84 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Silver/30991. ESC/ESC.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static long[][] mat = {{1,1,-1},{-2,1,0},{2,0,1}};
+    public static void main(String[] args) throws Exception {
+        long[][] ret = matPow(mat,Long.parseLong(br.readLine()));
+        bw.write(Long.toString(ret[0][0]+ret[0][1]+ret[0][2]));
+        bw.flush();
+    }
+    static long[][] matMul(long[][] a, long[][] b){
+        long[][] c = new long[b[0].length][a.length];
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                for(int k=0;k<b[0].length;k++){
+                    c[i][k] += a[i][j]*b[j][k];
+                }
+            }
+        }
+        return c;
+    }
+    static long[][] matPow(long[][] a, long num){
+        if(num==1) return a;
+        else if(num%2==1) return matMul(matPow(a,num-1),a);
+        else return matPow(matMul(a,a),num/2);
+    }
+}
