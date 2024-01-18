151935e806b4b5d483b78bf4264a83ff4c2e8e5a
RookMG <tngks1995@naver.com>
2024-01-18 21:20:17 +0900

    [Gold I] Title: 그림 교환, Time: 144 ms, Memory: 40160 KB -BaekjoonHub

--- 백준/Gold/1029. 그림 교환/그림 교환.java
+++ 백준/Gold/1029. 그림 교환/그림 교환.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,10 +9,7 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         dp = new int[10][max = 1<<N][N];
         cost = new int[N][N];
-        for(int r=0;r<N;r++){
-            String s = br.readLine();
-            for(int c=0;c<N;c++) cost[r][c] = s.charAt(c)-'0';
-        }
+        for(int r=0,c;(c=0)+r<N;r++) for(char ch:br.readLine().toCharArray()) cost[r][c++] = ch-'0';
         bw.write(Integer.toString(dfs(0,1,0)));
         bw.flush();
     }
@@ -22,7 +18,7 @@ public class Main {
         dp[price][visit][owner] = 1;
         if(visit == max) return dp[price][visit][owner];
         for(int i=0;i<N;i++){
-            if(i==owner||(visit&(1<<i))!=0||cost[owner][i]<price) continue;
+            if(i==owner||cost[owner][i]<price||(visit&(1<<i))!=0) continue;
             dp[price][visit][owner] = Math.max(dp[price][visit][owner],1+dfs(cost[owner][i],visit|(1<<i),i));
         }
         return dp[price][visit][owner];
