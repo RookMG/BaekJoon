e5e35dd69a3c1276d2a229b6eead25124ed89d17
RookMG <tngks1995@naver.com>
2024-01-21 18:46:40 +0900

    [Gold III] Title: 내리막 길, Time: 340 ms, Memory: 36304 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -6,36 +6,30 @@ public class Main {
     static StringTokenizer st;
     static final int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int R, C;
+    static int[][] arr, dp;
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
-        int[][][] arr = new int[R=Integer.parseInt(st.nextToken())][C=Integer.parseInt(st.nextToken())][2];
-        arr[0][0][1] = 1;
+        arr = new int[R=Integer.parseInt(st.nextToken())][C=Integer.parseInt(st.nextToken())];
+        dp = new int[R][C];
         for(int r=0;r<R;r++){
             st = new StringTokenizer(br.readLine());
-            for(int c=0;c<C;c++) arr[r][c][0] = Integer.parseInt(st.nextToken());
+            for(int c=0;c<C;c++) arr[r][c] = Integer.parseInt(st.nextToken());
+            Arrays.fill(dp[r],-1);
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return -Integer.compare(arr[o1[0]][o1[1]][0],arr[o2[0]][o2[1]][0]);
-            }
-        });
-        pq.offer(new int[]{0,0});
-        boolean[][] visit = new boolean[R][C];
-        while(!pq.isEmpty()){
-            int r = pq.peek()[0], c = pq.poll()[1], nr, nc;
-            if(visit[r][c]) continue;
-            visit[r][c] = true;
-            for(int d=0;d<4;d++){
-                if(!isIn(nr=r+delta[d][0],nc=c+delta[d][1])||arr[r][c][0]<=arr[nr][nc][0]) continue;
-                arr[nr][nc][1] += arr[r][c][1];
-                pq.offer(new int[]{nr,nc});
-            }
-        }
-        bw.write(Integer.toString(arr[R-1][C-1][1]));
+        bw.write(Integer.toString(dfs(R-1,C-1)));
         bw.flush();
     }
-
+    static int dfs(int r, int c){
+        if(r==0&&c==0) return 1;
+        if(dp[r][c]!=-1) return dp[r][c];
+        int ret = 0;
+        for(int d=0;d<4;d++){
+            int nr = r+delta[d][0], nc = c+delta[d][1];
+            if(!isIn(nr,nc)||arr[r][c]>=arr[nr][nc]) continue;
+            ret += dfs(nr,nc);
+        }
+        return dp[r][c] = ret;
+    }
     static boolean isIn(int r, int c) {
         return 0 <= r && r < R && 0 <= c && c < C;
     }
