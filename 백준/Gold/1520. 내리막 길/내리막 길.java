b763ee8e80ab9d1193e3a9f5ab88e12e2c53dc71
RookMG <tngks1995@naver.com>
2024-01-21 18:48:03 +0900

    [Gold III] Title: 내리막 길, Time: 328 ms, Memory: 39724 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -7,28 +7,29 @@ public class Main {
     static final int[][] delta = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static int R, C;
     static int[][] arr, dp;
+    static boolean[][] visit;
     public static void main(String[] args) throws Exception {
         st = new StringTokenizer(br.readLine());
         arr = new int[R=Integer.parseInt(st.nextToken())][C=Integer.parseInt(st.nextToken())];
         dp = new int[R][C];
+        visit = new boolean[R][C];
         for(int r=0;r<R;r++){
             st = new StringTokenizer(br.readLine());
             for(int c=0;c<C;c++) arr[r][c] = Integer.parseInt(st.nextToken());
-            Arrays.fill(dp[r],-1);
         }
         bw.write(Integer.toString(dfs(R-1,C-1)));
         bw.flush();
     }
     static int dfs(int r, int c){
         if(r==0&&c==0) return 1;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int ret = 0;
+        if(visit[r][c]) return dp[r][c];
         for(int d=0;d<4;d++){
             int nr = r+delta[d][0], nc = c+delta[d][1];
             if(!isIn(nr,nc)||arr[r][c]>=arr[nr][nc]) continue;
-            ret += dfs(nr,nc);
+            dp[r][c] += dfs(nr,nc);
         }
-        return dp[r][c] = ret;
+        visit[r][c] = true;
+        return dp[r][c];
     }
     static boolean isIn(int r, int c) {
         return 0 <= r && r < R && 0 <= c && c < C;
