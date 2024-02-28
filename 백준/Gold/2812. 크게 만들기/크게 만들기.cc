3aa9b9bdd870b4acd70a6eb886132d5ddc1664ad
RookMG <tngks1995@naver.com>
2024-02-28 10:48:53 +0900

    [Gold III] Title: 크게 만들기, Time: 12 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "deque"
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    int N, K;
+    cin >> N >> K;
+    string line;
+    cin >> line;
+    deque<char> stack;
+    for (int i = 0; i < N; i++) {
+        while (K != 0 && !stack.empty() && stack.back() < line[i]) {
+            stack.pop_back();
+            K--;
+        }
+        stack.push_back(line[i]);
+    }
+    while (K-- > 0) stack.pop_back();
+    while (!stack.empty()) {
+        cout << stack.front();
+        stack.pop_front();
+    }
+    return 0;
+}
