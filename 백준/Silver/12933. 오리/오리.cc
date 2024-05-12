39143302e7bedd4460e95e3d170cdb85836f961b
RookMG <tngks1995@naver.com>
2024-05-12 14:27:31 +0900

    [Silver II] Title: 오리, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/12933. 오리/오리.cc
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int ans = 0, sound[5] = {}, sum = 0;
+    char input[2501];
+    scanf("%s", input);
+    for (int i = 0; input[i] != '\0'; i++) {
+        int order;
+        switch (input[i]) {
+            case 'q':
+                order = 0;
+                break;
+            case 'u':
+                order = 1;
+                break;
+            case 'a':
+                order = 2;
+                break;
+            case 'c':
+                order = 3;
+                break;
+            case 'k':
+                order = 4;
+        }
+        ++sound[order];
+        if (order == 0) {
+            ans = ++sum > ans ? sum : ans;
+            continue;
+        }
+        if (--sound[order - 1] < 0) {
+            ans = -1;
+            break;
+        }
+        if (order == 4) --sum;
+    }
+    for (int i = 0; i < 4; i++) if (sound[i] > 0) ans = -1;
+    printf("%d", ans);
+}
