08965342d5e2c4df22e04e550cbe3b34df8f156b
RookMG <tngks1995@naver.com>
2024-06-10 18:52:14 +0900

    [Silver III] Title: 두 수의 합, Time: 224 ms, Memory: 25580 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static HashSet<Integer> set = new HashSet<>();
     static StringTokenizer st;
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine()), X, ans = 0;
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++) set.add(Integer.parseInt(st.nextToken()));
+        boolean[] set = new boolean[1000001];
+        for(int i=0;i<N;i++) set[Integer.parseInt(st.nextToken())] = true;
         X = Integer.parseInt(br.readLine());
-        for(int i=(X>>1) + 1;i<X;i++) if(set.contains(i)&&set.contains(X-i)) ans++;
+        for(int i=(X>>1) + 1;i<Math.min(X,1000000);i++) if(set[i]&&set[X-i]) ans++;
         bw.write(Integer.toString(ans));
         bw.flush();
     }
