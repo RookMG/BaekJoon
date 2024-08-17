b0eeb7743767dfe41574b13597caf9021619a947
RookMG <tngks1995@naver.com>
2024-08-17 13:38:47 +0900

    [Gold V] Title: 표 회전, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/2932. 표 회전/표 회전.cc
+++ 백준/Gold/2932. 표 회전/표 회전.cc
@@ -10,7 +10,7 @@ struct Move{
     int num, targetR, targetC, nowR, nowC;
 };
 
-vector<Move*> moves;
+vector<Move> moves;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -19,18 +19,18 @@ int main(){
     cin >> N >> K;
     for(int k=0,num,r,c;k<K;k++){
         cin >> num >> r >> c;
-        moves.push_back(new Move({num,r,c,(num-1)/N+1,(num-1)%N+1}));
+        moves.push_back({num,r,c,(num-1)/N+1,(num-1)%N+1});
     }
-    for(auto *el:moves){
-        int dr = (N+el->targetR-el->nowR)%N, dc = (N+el->targetC-el->nowC)%N;
+    for(auto &el:moves){
+        int dr = (N+el.targetR-el.nowR)%N, dc = (N+el.targetC-el.nowC)%N;
         cout << dr+dc <<'\n';
-        for(auto *elR:moves){
-            if(el->nowR!=elR->nowR) continue;
-            elR->nowC = (elR->nowC+dc-1)%N+1;
+        for(auto &elR:moves){
+            if(el.nowR!=elR.nowR) continue;
+            elR.nowC = (elR.nowC+dc-1)%N+1;
         }
-        for(auto *elC:moves){
-            if(el->nowC!=elC->nowC) continue;
-            elC->nowR = (elC->nowR+dr-1)%N+1;
+        for(auto &elC:moves){
+            if(el.nowC!=elC.nowC) continue;
+            elC.nowR = (elC.nowR+dr-1)%N+1;
         }
     }
 }
